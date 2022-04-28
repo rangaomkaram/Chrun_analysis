d3f65f633345d6e697a731789e22f4d342c94245
rangaomkaram <omkaram.ranga@gmail.com>
2022-04-28 10:45:44 +0530

    dataloader

--- Data_ingestion/data_loader.py
+++ Data_ingestion/data_loader.py
@@ -6,5 +6,32 @@ class Data_loader:
     This class defined to retrive the data from source or traning
 
     """
-    def __init__(self,file_object,logging_message):
-        self.training_file = 
+    def __init__(self,file_object,logging_object):
+        self.training_file = "telecom_Chrun_Analysis/TeleCom_Raw_Data/rawdata.csv"
+        self.file_object = file_object
+        self.logging_object = logging_object
+
+    def Get_Data(self):
+        """
+        Method Name : Get_Data
+        Description : This method reads the data from raw data source
+        Output : Pandas Dataframe
+    
+        """
+        self.logging_object.log(self.file_object,"Getting into the Get_Data Method of the Data_loader class")
+
+        """
+        Excepetion Handling 
+        """
+        try:
+            self.data = pd.read_csv(self.training_file)
+            self.logging_object.log(self.file_object,
+                        "Message : Raw Data is sucessfully loaded using Get_Data method of Data_loader class")
+            return self.data
+
+        except Exception as e:
+            self.logging_object.log(self.file_object,
+                                      "Excepection raise in the Get_Data method of the Data_loader class")
+            self.logging_object.log(self.file_object,
+                         "loading the Data is Unsuccessful.check the get_data method of the Data_loader class")
+            raise Exception()
